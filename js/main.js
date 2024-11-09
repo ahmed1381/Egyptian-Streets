e2b3522cab82afc7fd8ce43275966df0289ef49b
Ahmed Salah <tito.salah2004@gmail.com>
2024-11-09 20:22:13 +0200

    add website Egyptian Streets

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// window.alert(10*5);
+// document.write("<h1>AHMED</h1>");
+// console.log("Hello from Egyption streets");
+// console.log("Hello from %cEgyption streets", "color:red; font-size:2rem;");
+// var myname =" AHMED ";
+// console.log("Hello" + myname);
+// window.alert("Hello" + myname);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Data types intro
+//   ||
+//   VV
+
+//string
+//number
+//Array => object
+//object
+//Boolean
+
+console.log(typeof ("Ahmed"));  //name ==> string
+console.log(typeof 10000);  //money ==> number
+console.log(typeof [10, 15, 17]);  //number ==> object
+console.log(typeof ["Ahmed , mostafa ,omar"]); //name ==> object
+console.log(typeof {name: "Ahmed", age: 18, country: "egy"});  //object ==> object
+console.log(typeof true);  //Boolean ==> Boolean
+console.log(typeof false);  //Boolean ==> Boolean
+console.log(typeof undefined);
+console.log(typeof null);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//variables
+//syntax ( keyword | variables name | Assignment operator | variables value)
+
+var user = "mohamed", age = 19;
+
+console.log("Ahmed");  //change this name => console.log(user);   //change this name to number => console.log(age);
+console.log("Ahmed");  //change this name => console.log(user);   //change this name to number => console.log(age);
+console.log("Ahmed");  //change this name => console.log(user);   //change this name to number => console.log(age);
+console.log("Ahmed");  //change this name => console.log(user);   //change this name to number => console.log(age);
+id.innerHTML = "Option";  // change any text from html bage used id =>           this (id) = id name
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
